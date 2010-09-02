315fe54c45119b028eabc363a35a8e947218f4bb
Rony <rony.yeung@gmail.com>
2010-09-02 22:04:46 +0800

    etc-gentoo/udev/rules.d/65-kvm.rules add/update

--- etc-gentoo/profile.d/my.sh
+++ etc-gentoo/profile.d/my.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
 export LANG=en_US.UTF-8
+
+alias df='df -h'
+alias f='free -m'
+alias ll='ls -la'
+alias m='mount '
+
+alias es='eix-sync && eix-update'
+alias eu='etc-update && env-update && . /etc/profile'
+alias ew='emerge -uDNpv world'
+
