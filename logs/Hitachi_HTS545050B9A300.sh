68bad235cf9c4f3d2247b04c6caa2591437b519b
Rony <devnull@localhost>
2011-01-31 14:02:28 +0800

    S.M.A.R.T. Hitachi 2.5" 500GB updated

--- logs/Hitachi_HTS545050B9A300.sh
+++ logs/Hitachi_HTS545050B9A300.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-smartctl -a /dev/sda > Hitachi_HTS545050B9A300 \
+sudo smartctl -a /dev/sda > Hitachi_HTS545050B9A300 \
   && hg commit -A -m 'S.M.A.R.T. Hitachi 2.5" 500GB updated' \
   && hg push
